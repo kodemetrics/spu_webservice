b4d8c20b2a5e6dd072d53f45ca090f66740a7dda
kodemetrics <okaforwilson2@gmail.com>
2024-04-26 08:26:55 +0100

    src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,17 +28,8 @@ struct AppState {
 
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
 
 // #[post("/save-user")]
@@ -65,9 +56,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(app_data.clone())
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(index)
             //.service(create_info)
             .configure(config)
     })
